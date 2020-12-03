d210523cafaac80bda82da529540eaf5eaf83999
SSONNGG <1647478489@qq.com>
2020-12-03 13:03:05 +0800

    phomark基础项目上传

---
+++ src/main/java/com/gsn/phonemark/PhonemarkApplication.java
@@ -0,0 +1,13 @@
+package com.gsn.phonemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonemarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonemarkApplication.class, args);
+    }
+
+}
