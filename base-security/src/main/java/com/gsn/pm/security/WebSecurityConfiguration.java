f0d79dfb2a00a75e70076990326f3ceaf38e45f8
ZOU8034 <2754492230@qq.com>
2020-12-07 20:55:32 +0800

    Eureka基础服务、Config服务、Dashboard服务、Turbine服务、Zuul服务

---
+++ base-security/src/main/java/com/gsn/pm/security/WebSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.gsn.pm.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity   //  启用安全
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder()).withUser("root")
+                .password(new BCryptPasswordEncoder().encode("a"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("a"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests().anyRequest()
+                .fullyAuthenticated();
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/actuator/hystrix.stream","/turbine.stream") ;
+    }
+}
